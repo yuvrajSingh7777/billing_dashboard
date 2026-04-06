20ad6e4bd81ecadfab6ccc4bc37f6799e8242d35
yuvrajSingh7777 <168898701+yuvrajSingh7777@users.noreply.github.com>
2026-04-07 00:10:16 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      }
+   
     }
   }
 })
